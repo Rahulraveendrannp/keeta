df0489df9ae64c1ee18c159bfd7a97b60c2efd78
Akhil Varghese <124122740+akhil-pumex@users.noreply.github.com>
2025-11-23 12:05:50 +0530

    changes for deployment

--- client/src/api.ts
+++ client/src/api.ts
@@ -9,7 +9,7 @@ import type {
 
 const API_BASE =
   (import.meta as any).env?.VITE_API_BASE ||
-  "https://talabat-scavenger-hunt-backend-657638641053.us-central1.run.app/api";
+  "https://keeta-be-657638641053.us-central1.run.app/api";
 
 function setToken(token: string) {
   localStorage.setItem("jwt_token", token);
